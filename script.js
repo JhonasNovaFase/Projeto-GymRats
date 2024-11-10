2b0cb0d069dec8911104d3b7d61c709e4e1669cf
João Victor Braga Reis <110208420+JhonasNovaFase@users.noreply.github.com>
2024-11-10 18:41:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function adicionarAoCarrinho(produtoId) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    carrinho.push(produtoId);
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    alert('Produto adicionado ao carrinho!');
+}
+
 function exibirCarrinho() {
     const carrinhoLista = document.getElementById('carrinho-lista');
     const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
@@ -8,42 +15,9 @@ function exibirCarrinho() {
     });
 }
 function finalizarCompra() {
-    alert('Obrigado por comprar na GymRats!');
+    alert('Compra finalizada!');
     localStorage.removeItem('carrinho');
     window.location.href = 'index.html';
 }
 
 exibirCarrinho();
-function obterCarrinho() {
-    return JSON.parse(localStorage.getItem('carrinho')) || [];
-}
-
-function atualizarCarrinho(carrinho) {
-    localStorage.setItem('carrinho', JSON.stringify(carrinho));
-}
-
-function existeNoCarrinho(produtoId) {
-    const carrinho = obterCarrinho();
-    return carrinho.includes(produtoId);
-}
-
-function adicionarAoCarrinho(produtoId) {
-    if (existeNoCarrinho(produtoId)) {
-        alert('Este produto já está no carrinho.');
-        return; 
-    }
-
-    const carrinho = obterCarrinho();
-    carrinho.push(produtoId);
-    atualizarCarrinho(carrinho);
-
-    alert(`Produto "${produtoId}" adicionado ao carrinho com sucesso!`);
-}
-
-
-//function adicionarAoCarrinho(produtoId) { 
-            //let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-            //carrinho.push(produtoId);
-            //localStorage.setItem('carrinho', JSON.stringify(carrinho));
-            //alert('Produto adicionado ao carrinho!');
-        //}
